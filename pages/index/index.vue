4ba17bc002c54f4514d092b78a5f16e02208bf52
chengyamin <chengyamimxiao@163.com>
2019-08-03 19:04:10 +0800

    图文视频列表样式 封装列表样式组件 滚动导航Tab 封装滚动导航tab组件 上拉加载更多及封装上拉加载更多

---
+++ pages/index/index.vue
@@ -0,0 +1,440 @@
+<template> 
+	<view>
+		
+		
+		<!-- 顶部选项卡 -->
+		<swiper-tab-head :tabBars="tabBars" :tabIndex="tabIndex" @tabtap="tabtap"></swiper-tab-head>
+		<!--  -->
+		<view class="uni-tab-bar">
+			<swiper class="swiper-box" :style="{height:swiperHeight+'px'}" :current="tabIndex" @change="tabChange">
+				<swiper-item v-for="(items,index) in newslist" :key="index">
+					<scroll-view scroll-y class="list" @scrolltolower="loadmore(index)">
+						<!-- 图文列表 -->
+						<block v-for="(item,index1) in items.list" :key="index1">
+							<index-list :item="item" :index="index1"></index-list>
+						</block>
+						<!-- 上拉加载更多 -->
+						<load-more :loadMore="items.list.loadtext"></load-more>
+					</scroll-view>
+				</swiper-item>
+			</swiper>
+		</view>
+		
+		
+	</view>
+</template>
+
+<script>
+	import indexList from "../../components/index/index-list.vue"
+	import swiperTabHead from "../../components/index/swiper-tab-head.vue"
+	import loadMore from '../../components/common/load-more.vue';
+	export default {
+		components:{
+			indexList,
+			swiperTabHead,
+			loadMore
+		},
+		data() {
+			return {
+				swiperHeight:500,
+				tabIndex:0,
+				tabBars:[
+					{
+						name:"关注",id:"guanzhu"
+					},
+					{
+						name:"推荐",id:"tuijian"
+					},
+					{
+						name:"体育",id:"tiyu"
+					},
+					{
+						name:"热点",id:"redian"
+					},
+					{
+						name:"财经",id:"caijing"
+					},
+					{
+						name:"娱乐",id:"yule"
+					}
+				],
+				newslist:[
+					{
+						loadtext:"上拉加载更多",
+						list:[
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:false,
+								title:"我是标题",
+								type:"img",//img图文 video视频
+								titlepic:"../../static/demo/datapic/11.jpg",//封面图
+								infonum:{
+									index:0,//0没有操作  1已经顶了 2已经踩了
+									dingnum:11,
+									cainum:11
+								},
+								commentnum:10,//评论
+								sharenum:10
+							},
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:true,
+								title:"我是标题",
+								type:"video",//img图文 video视频
+								titlepic:"../../static/demo/datapic/11.jpg",//封面图
+								playnum:"20w",
+								long:"2:47",
+								infonum:{
+									index:1,//0没有操作  1已经顶了 2已经踩了
+									dingnum:11,
+									cainum:11
+								},
+								commentnum:10,//评论
+								sharenum:10
+							}
+						]
+					},
+					{
+						loadtext:"上拉加载更多",
+						list:[
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:false,
+								title:"我是标题",
+								type:"img",//img图文 video视频
+								titlepic:"../../static/demo/datapic/11.jpg",//封面图
+								infonum:{
+									index:0,//0没有操作  1已经顶了 2已经踩了
+									dingnum:11,
+									cainum:11
+								},
+								commentnum:10,//评论
+								sharenum:10
+							},
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:true,
+								title:"我是标题",
+								type:"video",//img图文 video视频
+								titlepic:"../../static/demo/datapic/11.jpg",//封面图
+								playnum:"20w",
+								long:"2:47",
+								infonum:{
+									index:1,//0没有操作  1已经顶了 2已经踩了
+									dingnum:11,
+									cainum:11
+								},
+								commentnum:10,//评论
+								sharenum:10
+							},
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:false,
+								title:"我是标题",
+								type:"img",//img图文 video视频
+								titlepic:"../../static/demo/datapic/11.jpg",//封面图
+								infonum:{
+									index:0,//0没有操作  1已经顶了 2已经踩了
+									dingnum:11,
+									cainum:11
+								},
+								commentnum:10,//评论
+								sharenum:10
+							},
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:true,
+								title:"我是标题",
+								type:"video",//img图文 video视频
+								titlepic:"../../static/demo/datapic/11.jpg",//封面图
+								playnum:"20w",
+								long:"2:47",
+								infonum:{
+									index:1,//0没有操作  1已经顶了 2已经踩了
+									dingnum:11,
+									cainum:11
+								},
+								commentnum:10,//评论
+								sharenum:10
+							}
+						]
+					},
+					{
+						loadtext:"上拉加载更多",
+						list:[
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:false,
+								title:"我是标题",
+								type:"img",//img图文 video视频
+								titlepic:"../../static/demo/datapic/11.jpg",//封面图
+								infonum:{
+									index:0,//0没有操作  1已经顶了 2已经踩了
+									dingnum:11,
+									cainum:11
+								},
+								commentnum:10,//评论
+								sharenum:10
+							},
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:true,
+								title:"我是标题",
+								type:"video",//img图文 video视频
+								titlepic:"../../static/demo/datapic/11.jpg",//封面图
+								playnum:"20w",
+								long:"2:47",
+								infonum:{
+									index:1,//0没有操作  1已经顶了 2已经踩了
+									dingnum:11,
+									cainum:11
+								},
+								commentnum:10,//评论
+								sharenum:10
+							},
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:false,
+								title:"我是标题",
+								type:"img",//img图文 video视频
+								titlepic:"../../static/demo/datapic/11.jpg",//封面图
+								infonum:{
+									index:0,//0没有操作  1已经顶了 2已经踩了
+									dingnum:11,
+									cainum:11
+								},
+								commentnum:10,//评论
+								sharenum:10
+							},
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:true,
+								title:"我是标题",
+								type:"video",//img图文 video视频
+								titlepic:"../../static/demo/datapic/11.jpg",//封面图
+								playnum:"20w",
+								long:"2:47",
+								infonum:{
+									index:1,//0没有操作  1已经顶了 2已经踩了
+									dingnum:11,
+									cainum:11
+								},
+								commentnum:10,//评论
+								sharenum:10
+							},
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:false,
+								title:"我是标题",
+								type:"img",//img图文 video视频
+								titlepic:"../../static/demo/datapic/11.jpg",//封面图
+								infonum:{
+									index:0,//0没有操作  1已经顶了 2已经踩了
+									dingnum:11,
+									cainum:11
+								},
+								commentnum:10,//评论
+								sharenum:10
+							},
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:true,
+								title:"我是标题",
+								type:"video",//img图文 video视频
+								titlepic:"../../static/demo/datapic/11.jpg",//封面图
+								playnum:"20w",
+								long:"2:47",
+								infonum:{
+									index:1,//0没有操作  1已经顶了 2已经踩了
+									dingnum:11,
+									cainum:11
+								},
+								commentnum:10,//评论
+								sharenum:10
+							}
+						]
+					},
+					{
+						loadtext:"上拉加载更多",
+						list:[
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:false,
+								title:"我是标题",
+								type:"img",//img图文 video视频
+								titlepic:"../../static/demo/datapic/11.jpg",//封面图
+								infonum:{
+									index:0,//0没有操作  1已经顶了 2已经踩了
+									dingnum:11,
+									cainum:11
+								},
+								commentnum:10,//评论
+								sharenum:10
+							},
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:true,
+								title:"我是标题",
+								type:"video",//img图文 video视频
+								titlepic:"../../static/demo/datapic/11.jpg",//封面图
+								playnum:"20w",
+								long:"2:47",
+								infonum:{
+									index:1,//0没有操作  1已经顶了 2已经踩了
+									dingnum:11,
+									cainum:11
+								},
+								commentnum:10,//评论
+								sharenum:10
+							}
+						]
+					},
+					{
+						loadtext:"上拉加载更多",
+						list:[
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:false,
+								title:"我是标题",
+								type:"img",//img图文 video视频
+								titlepic:"../../static/demo/datapic/11.jpg",//封面图
+								infonum:{
+									index:0,//0没有操作  1已经顶了 2已经踩了
+									dingnum:11,
+									cainum:11
+								},
+								commentnum:10,//评论
+								sharenum:10
+							},
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:true,
+								title:"我是标题",
+								type:"video",//img图文 video视频
+								titlepic:"../../static/demo/datapic/11.jpg",//封面图
+								playnum:"20w",
+								long:"2:47",
+								infonum:{
+									index:1,//0没有操作  1已经顶了 2已经踩了
+									dingnum:11,
+									cainum:11
+								},
+								commentnum:10,//评论
+								sharenum:10
+							}
+						]
+					},
+					{
+						loadtext:"上拉加载更多",
+						list:[
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:false,
+								title:"我是标题",
+								type:"img",//img图文 video视频
+								titlepic:"../../static/demo/datapic/11.jpg",//封面图
+								infonum:{
+									index:0,//0没有操作  1已经顶了 2已经踩了
+									dingnum:11,
+									cainum:11
+								},
+								commentnum:10,//评论
+								sharenum:10
+							},
+							{
+								userpic:"../../static/demo/userpic/12.jpg",
+								username:"昵称",
+								isguanzhu:true,
+								title:"我是标题",
+								type:"video",//img图文 video视频
+								titlepic:"../../static/demo/datapic/11.jpg",//封面图
+								playnum:"20w",
+								long:"2:47",
+								infonum:{
+									index:1,//0没有操作  1已经顶了 2已经踩了
+									dingnum:11,
+									cainum:11
+								},
+								commentnum:10,//评论
+								sharenum:10
+							}
+						]
+					}
+				],
+			}
+		},
+		onLoad() {
+			uni.getSystemInfo({
+				success:(res)=>{
+					// console.log(res.windowHeight);
+					let height=res.windowHeight-uni.upx2px(100);
+					console.log(height);
+					this.swiperHeight=height;
+				}
+			});
+		},
+		methods: {
+			//上拉加载
+			loadmore(index){
+				if(this.newslist[index].loadtext!="上拉加载更多"){
+					return;
+				}
+				//修改状态
+				this.newslist[index].loadtext="加载中...";
+				//获取数据
+				setTimeout(()=>{
+					//获取完成
+					let obj={
+						userpic:"../../static/demo/userpic/12.jpg",
+						username:"昵称",
+						isguanzhu:false,
+						title:"我是标题",
+						type:"img",//img图文 video视频
+						titlepic:"../../static/demo/datapic/11.jpg",//封面图
+						infonum:{
+							index:0,//0没有操作  1已经顶了 2已经踩了
+							dingnum:11,
+							cainum:11
+						},
+						commentnum:10,//评论
+						sharenum:10
+					};
+					this.newslist[index].list.push(obj);
+					this.newslist[index].loadtext="上拉加载更多";
+				},1000)
+				// this.newslist[index].loadtext="没有更多数据了";
+			},
+			tabtap(index){
+				//tabbar点击事件
+				console.log(index);
+				this.tabIndex=index;
+			},
+			tabChange(e){
+				//滑动事件
+				console.log(JSON.stringify(e.detail));
+				this.tabIndex=e.detail.current;
+			}
+		}
+	}
+</script>
+
+<style>
+	
+</style>
